bcd762103c2c745d9c21dd7726a32af1d50f9ade
Tarik <95280912+TarikLaanaya@users.noreply.github.com>
2025-10-18 01:40:17 +0100

    Added basic horizontal movement
    Player can now move in horizontal space

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public bool acceptInput = true;
+
+    //[SerializeField] private KeyCode pauseKey = KeyCode.Escape; // Example of a serialized field for a pause key
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // Check for different inputs
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float maxSpeed;
+    private Rigidbody rb;
+    Vector3 moveDirection;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3(moveX, 0, moveZ);
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector3(moveDirection.x * maxSpeed, rb.linearVelocity.y, moveDirection.z * maxSpeed);
+    }
+}
