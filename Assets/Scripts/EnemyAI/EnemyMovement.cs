ad38faa93a38b6180f65865fd2a5847299ca4382
Tarik <95280912+TarikLaanaya@users.noreply.github.com>
2025-10-23 15:18:05 +0100

    Added additional target location logic
    Enemy now finds a location away from the player instead of going inside of them

--- Assets/Scripts/EnemyAI/EnemyMovement.cs
+++ Assets/Scripts/EnemyAI/EnemyMovement.cs
@@ -5,6 +5,7 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField] private float maxSpeed;
     [SerializeField] private float accelDeccel;
     [SerializeField] private Transform playerTransform;
+    [SerializeField] private float attackDistanceFromPlayer;
     private Rigidbody rb;
     private EnemyAINavigation enemyAINavigation;
 
@@ -22,8 +23,16 @@ public class EnemyMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 dir = enemyAINavigation.TargetDirection(playerTransform.position); // get target direction from EnemyAINavigation
+        Vector3 dir = enemyAINavigation.TargetDirection(LocationAwayFromPlayer(attackDistanceFromPlayer)); // get direction from EnemyAINavigation using our target
 
         rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, dir.normalized * maxSpeed, accelDeccel * Time.deltaTime); // Apply movement with max speed
+    }
+
+    Vector3 LocationAwayFromPlayer(float distance) // Find a location a certain distance away from the player
+    {
+        Vector3 dirFromPlayer = (transform.position - playerTransform.position).normalized; //get direction
+        Vector3 newPosition = playerTransform.position + dirFromPlayer * distance; // create new position from direction and distance
+
+        return newPosition;
     }
 }
