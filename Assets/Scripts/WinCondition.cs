a7227561875948ccf3d115059c745323173f2370
Sam Jones <2504224@uad.ac.uk>
2025-11-12 19:44:28 +0000

    Victory HUD added

--- Assets/Scripts/WinCondition.cs
+++ Assets/Scripts/WinCondition.cs
@@ -1,18 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class WinCondition : MonoBehaviour
 {
     [SerializeField] private GameObject[] enemyBases;
     private int enemies;
+    public Image Crosshair;
+    public Image Minigun;
+    public Image ChargeGun;
+    public Image Missile;
+    public Image MissileGrey;
+    public Canvas VictoryCanvas;
+    public Button VictoryReturnToMenu;
+
 
     void Start()
     {
+        VictoryCanvas.enabled = false;
+        Crosshair.enabled = true;
+        Minigun.enabled = true;
+        ChargeGun.enabled = true;
+        Missile.enabled = true;
         foreach (GameObject enemyBase in enemyBases)
         {
             EnemyManager enemyManager = enemyBase.GetComponent<EnemyManager>();
             enemies += enemyManager.initialEnemyCount;
             enemyManager.winCondition = this;
         }
+    }
+
+    public void ReturnToMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("TitleMenu");
     }
 
     public void EnemyDestroyed()
@@ -22,7 +42,14 @@ public class WinCondition : MonoBehaviour
         if (enemies <= 0)
         {
             //YOU WIN
+            VictoryCanvas.enabled = true;
+            Crosshair.enabled = false;
+            Minigun.enabled = false;
+            ChargeGun.enabled = false;
+            Missile.enabled = false;
             Debug.Log("WIN");
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
         }
     }
 }
